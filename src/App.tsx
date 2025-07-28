7f2ab4e10991ba73e9f97c93c40abe58d911e279
thanhtrung07 <40331826+thanhtrung07@users.noreply.github.com>
2025-07-28 23:01:51 +0700

    feat[ECV-1]: Init base configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from './components/ui/button'
+import ReactSvg from './assets/react.svg?react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log('>>>> import.meta.env.MODE', import.meta.env.MODE)
+  console.log('>>>> import.meta.env.BASE_URL', import.meta.env.BASE_URL)
+  console.log('>>>> import.meta.env.PROD', import.meta.env.PROD)
+  console.log('>>>> import.meta.env.DEV', import.meta.env.DEV)
+  console.log('>>>> import.meta.env.SSR', import.meta.env.SSR)
+  console.log(
+    '>>>> import.meta.env.VITE_SOME_KEY',
+    import.meta.env.VITE_SOME_KEY
+  )
+  console.log(
+    '>>>> import.meta.env.DB_PASSWORD',
+    import.meta.env.DB_PASSWORD + 'string'
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p className="text-amber-500">Hello</p>
+      <Button asChild>
+        <span>test</span>
+      </Button>
+      <ReactSvg className="size-16" />
+    </div>
   )
 }
 
